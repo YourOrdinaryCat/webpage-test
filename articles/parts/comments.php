9f97df4e642b4cd194ac2b893d444eac6b7807e0
Omar Plata Salas <omarps464@gmail.com>
2021-03-02 18:48:28 -0500

    fix small bug

--- articles/parts/comments.php
+++ articles/parts/comments.php
@@ -4,7 +4,9 @@
     <div class="comments">
       <script async src="https://comments.app/js/widget.js?3" data-comments-app-website="B8gkNf6d" data-limit="100" data-height="397" data-dislikes="1" data-outlined="1"></script>
     </div>');
-  } else {
+  }
+
+  if($_SESSION['comments'] = "dark") {
     echo('<h2>Comments</h2>
     <div class="comments">
       <script async src="https://comments.app/js/widget.js?3" data-comments-app-website="B8gkNf6d" data-limit="100" data-height="397" data-dislikes="1" data-outlined="1" data-dark="1"></script>
